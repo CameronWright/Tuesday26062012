f637eaf8ca80c9f5cfdc57c01cea83481b10cbc4
Cameron Wright <fathobbitdotcom@gmail.com>
2012-06-27 20:29:42 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsBootstrapDeviseCancan::Application.config.secret_token = '6b63c9d58c77f9bb55017767bd85a2d4b4497d5afcc93afe041f6b821b9ba95ae5070db486ff4d3d0a6465512f9792784fac2e9b55e66871565d2ace6b81403a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RailsBootstrapDeviseCancan::Application.config.session_store :cookie_store, key: '_rails_bootstrap_devise_cancan_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RailsBootstrapDeviseCancan::Application.config.session_store :active_record_store
